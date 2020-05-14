7308a1fe30b22d7983ce9bcba6a3c713b38c0a1e
Danny <dgpeon62@gmail.com>
2020-05-14 19:05:59 -0400

    Added blog detail html

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return (self.body[:100] + '.....')
+        #return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e, %Y')
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
 
 # Create a Blog model
@@ -7,11 +23,6 @@ from django.db import models
 #   pub_date
 #   body
 #   image
-class Blog(models.Model):
-    title = models.CharField(max_length=255)
-    pub_date = models.DateTimeField()
-    body = models.TextField()
-    image = models.ImageField(upload_to='images/')
 
 # Add the Blog app to the settings
 
