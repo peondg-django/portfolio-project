073d8c74d9f033fb8a808eeab75ae0718a8b1168
Danny <dgpeon62@gmail.com>
2020-07-29 11:19:17 -0400

    Added local support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,6 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
+        'PASSWORD': 'domon39',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -133,3 +134,8 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
